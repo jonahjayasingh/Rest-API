0598042822a4f353b4546dc48c17bbc88c871c50
Jonah Jayasingh <jayasinghjonah24@gmail.com>
2023-11-21 18:14:17 +0530

    data fetching

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,15 +1,16 @@
-from django.http import JsonResponse
-import json
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from django.forms.models import model_to_dict
+from products.models import Product
+from products.serializers import ProductSerializers
 
 
+@api_view(["POST"])
 def api_home(request,*args,**kwargs):
-    body = request.body
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    try:
-        data = json.loads(body)
-    except:
-        pass
-    data["header"] = dict(request.headers)
-    data["parms"] = dict(request.GET)
-    data["content_type"] = request.content_type
-    return JsonResponse(data)
+    if instance :
+        data = ProductSerializers(instance).data
+        # data = model_to_dict(model_data,fields=["id","title","price","sale_price"])
+    return Response(data)
