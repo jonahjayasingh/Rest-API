f08ecc0d5be9894ada839204a7cae665e2210d73
Jonah Jayasingh <81973624+jonahjayasingh@users.noreply.github.com>
2024-11-10 20:19:50 +0530

    Delete py_client directory

--- py_client/basic.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-endpoint ="http://www.httpbin.org/anything"
-endpoint ="http://127.0.0.1:8000"
-
-data = requests.get(endpoint,params={"abc":123},json={"query":"Hello World"})
-print(data.json())
